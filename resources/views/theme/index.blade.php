1cc5e415c361825c95a73bd3af4304c7cf5adf6d
HMisawa3 <mintia.jaxa@gmail.com>
2021-09-01 18:54:33 +0900

    footerつくた

--- resources/views/theme/index.blade.php
+++ resources/views/theme/index.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>掲示板</title>
 </head>
+
 <body>
 <!-- お題 -->
 <div class="card mb-3">
@@ -20,7 +21,7 @@
       <div class="card-body">
         <h5 class="card-title">{{ $theme ->title }}</h5>
         <p class="card-text">{{ $theme ->description }}</p>
-        <a href="{{ $theme ->url }}" class="btn btn-primary">詳しくはこちら</a>
+        <a href="{{ $theme ->url }}" class="btn btn-secondary">詳しくはこちら</a>
       </div>
     </div>
   </div>
@@ -31,7 +32,7 @@
 {{ csrf_field() }}
   <div class="form-group row mr-2 ml-2">
     <input class="form-control col-11" type="text" placeholder="〇〇字以内で入力して下さい" name="post" required>
-    <button type="submit" class="btn btn-primary col-1" name="theme_id" value="{{ $theme ->id }}">投稿</button>
+    <button type="submit" class="btn btn-secondary" name="theme_id" value="{{ $theme ->id }}" >投稿</button>
   </div>
 </form>
 <!-- お題に対するコメント -->
@@ -44,6 +45,50 @@
     </div>
 </div>
 @endforeach
+
+<footer class="bg-dark u-content-space-top pb-4" role="footer">
+      <div class="container-fluid">
+        <div class="px-md-3">
+          <!-- Social Sharing -->
+          <div class="text-center">
+          <h2 class="h5 font-weight-light text-white mb-4">Thanx using gazel. We are grad.</h2>
+
+            <div class="d-flex justify-content-center">
+              <!-- Facebook Share -->
+              <div class="mr-3 mb-2 mb-md-0">
+                <div class="fb-share-button demo-faceook-share"
+                     data-href="http://facebook.com/htmlstream"
+                     data-layout="button"
+                     data-size="large"
+                     data-mobile-iframe="true">
+                  <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">FacebookShare</a>
+                </div>
+              </div>
+              <!-- End Facebook Share -->
+
+              <!-- Tweet -->
+              <div class="mr-3">
+                <a href="https://twitter.com/intent/tweet" class="twitter-share-button" data-size="large" data-text="Stream UI Kit is beautiful Open Source Bootstrap 4 UI Kit under MIT license." data-hashtags="StreamUIKit, Bootstrap, Freebies" data-related="htmlstream, freebies, bootstrap">Tweet</a>
+              </div>
+              <!-- End Tweet -->
+            </div>
+          </div>
+          <!-- End Social Sharing -->
+
+          <hr class="u-opacity-1 mb-4">
+        </div>
+
+        <!-- Copyright and Social Icons -->
+        <div class="row px-md-3">
+          <div class="col-md-8 text-center text-md-left mb-3 mb-md-0">
+            <small class="text-white">&copy; 2021 <a class="text-white" href="https://htmlstream.com">gazzel</a>. </small>
+          </div>
+
+        </div>
+        <!-- End Copyright and Social Icons -->
+      </div>
+</footer>
+
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
