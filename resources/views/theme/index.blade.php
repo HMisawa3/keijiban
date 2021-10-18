fbf426ed74bff2a9bba59a1d08ecd5dee382fdf4
HMisawa3 <mintia.jaxa@gmail.com>
2021-10-18 17:01:57 +0900

    デザインやり直し

--- resources/views/theme/index.blade.php
+++ resources/views/theme/index.blade.php
@@ -5,7 +5,7 @@
 <div class="card mb-3">
   <div class="row no-gutters">
     <div class="col-md-4">
-     <img src="{{ $theme -> image }}" alt="{{ $theme ->title }}">
+     <img src="{{ $theme -> image }}" alt="{{ $theme ->title }}" width="100%" height="100%">
     </div>
     <div class="col-md-8">
       <div class="card-body">
